8a7dd15479c6b5dbe3ae9f280477ace9dfd4354c
bqqbarbhg <samuli.1995@hotmail.com>
2015-12-02 17:13:26 +0200

    02/part2: Stream the input

--- 01/part2.c
+++ 01/part2.c
@@ -2,23 +2,28 @@
 
 int main(int argc, char **argv)
 {
-	static char input[16 * 1024] = { 0 };
-	size_t length = fread(input, 1, sizeof(input), stdin);
-
 	int floor = 0;
 
-	for (size_t i = 0; i < length; i++) {
-		char c = input[i];
+	char input[1024];
+	int base = 1;
 
-		unsigned relative = c - '(';
-		if (relative < 2) {
-			floor -= relative * 2 - 1;
+	while (!feof(stdin)) {
+		size_t length = fread(input, 1, sizeof(input), stdin);
 
-			if (floor < 0) {
-				printf("%d\n", (int)(i + 1));
-				return 0;
+		for (size_t i = 0; i < length; i++) {
+
+			unsigned relative = input[i] - '(';
+			if (relative < 2) {
+				floor -= relative * 2 - 1;
+
+				if (floor < 0) {
+					printf("%d\n", (int)(base + i));
+					return 0;
+				}
 			}
 		}
+
+		base += (int)length;
 	}
 	return 1;
 }
