69dfa8a2982d581b235d5dca27ec77090f0020b6
bqqbarbhg <samuli.1995@hotmail.com>
2015-12-02 16:21:14 +0200

    Solved day 1 part 1

---
+++ 01/main.c
@@ -0,0 +1,29 @@
+#include <xmmintrin.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	static char input[16 * 1024] __attribute__((aligned(16))) = { 0 };
+
+	__m128i open = _mm_set1_epi8('(');
+	__m128i close = _mm_set1_epi8(')');
+
+	size_t length = fread(input, 1, sizeof(input), stdin);
+
+	int floor = 0;
+
+	for (size_t block = 0; block < length; block += 16) {
+		__m128i input_block = _mm_load_si128((const __m128i*)(input + block));
+
+		__m128i open_block = _mm_cmpeq_epi8(input_block, open);
+		__m128i close_block = _mm_cmpeq_epi8(input_block, close);
+
+		int opens = _mm_movemask_epi8(open_block);
+		int closes = _mm_movemask_epi8(close_block);
+
+		floor += __builtin_popcount(opens) - __builtin_popcount(closes);
+	}
+
+	printf("%d\n", floor);
+}
+
