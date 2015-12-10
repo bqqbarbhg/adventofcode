e9cfde8113cf4df0cf4001e2768bc22ea1c4c002
bqqbarbhg <samuli.1995@hotmail.com>
2015-12-10 15:37:06 +0200

    Solved day 10

---
+++ 10/part1.hs
@@ -0,0 +1,14 @@
+import Data.List
+import Data.Maybe
+
+seesay [] = ""
+seesay (x:xs) = let index = (fromMaybe (length xs)) (findIndex (/= x) xs) in
+    show (index + 1) ++ [x] ++ seesay (drop index xs)
+
+seesays 0 x = x
+seesays n x = seesay (seesays (n - 1) x)
+
+main = do
+    line <- getLine
+    putStrLn ((show . length . seesays 40) line)
+
