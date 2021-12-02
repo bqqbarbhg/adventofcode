11e726a2e0aeaec62a9a17d099e0033026eb6251
Samuli Raivio <bqqbarbhg@gmail.com>
2021-12-02 19:59:37 +0200

    2021: Solve day 2

---
+++ 2021/02/part2.zig
@@ -0,0 +1,69 @@
+const std = @import("std");
+
+const Op = enum {
+    forward,
+    up,
+    down,
+};
+
+const Cmd = struct {
+    op: Op,
+    arg: i32,
+};
+
+const State = struct {
+    x: i32,
+    y: i32,
+    aim: i32,
+};
+
+pub fn parseOperation(name: []const u8) ?Op {
+    inline for (@typeInfo(Op).Enum.fields) |field, i| {
+        if (std.mem.eql(u8, name, field.name)) {
+            return @intToEnum(Op, i);
+        }
+    }
+    return null;
+}
+
+pub fn parseCommand(line: []const u8) !Cmd {
+    const mid = std.mem.indexOfScalar(u8, line, ' ') orelse return error.BadSyntax;
+    return Cmd{
+        .op = parseOperation(line[0..mid]) orelse return error.UnknownOperation,
+        .arg = try std.fmt.parseInt(i32, line[mid+1..], 10),
+    };
+}
+
+pub fn main() !void {
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
+    const stderr = std.io.getStdErr().writer();
+
+    var state = State{ .x = 0, .y = 0, .aim = 0 };
+
+    var buf: [256]u8 = undefined;
+    var lineno: u32 = 0;
+    while (try stdin.readUntilDelimiterOrEof(buf[0..], '\n')) |rawLine| {
+        lineno += 1;
+        const line = std.mem.trim(u8, rawLine, std.ascii.spaces[0..]);
+
+        const cmd = parseCommand(line) catch |err| {
+            try stderr.print("Error on line {}: {s}\n", .{ lineno, @errorName(err) });
+            continue;
+        };
+
+        const arg = cmd.arg;
+        switch (cmd.op) {
+            .up => state.aim -= arg,
+            .down => state.aim += arg,
+            .forward => {
+                state.x += arg;
+                state.y += arg * state.aim;
+            },
+        }
+    }
+
+    const result = state.x * state.y;
+    try stdout.print("{}\n", .{result});
+}
+
