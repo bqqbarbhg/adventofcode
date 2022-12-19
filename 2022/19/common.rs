c2cc03fda4fdf12ade575a129639d4052008039c
Samuli Raivio <bqqbarbhg@gmail.com>
2022-12-19 19:56:06 +0200

    2022/19: Optimization stolen from Reddit
    It's so obvious in hindsight... 4s -> 0.1s

--- 2022/19/common.rs
+++ 2022/19/common.rs
@@ -44,12 +44,31 @@ impl Resources {
         }
     }
 
+    pub fn get(&self, r: Resource) -> i32 {
+        use Resource::*;
+        match r {
+            Ore => self.ore,
+            Clay => self.clay,
+            Obsidian => self.obsidian,
+            Geode => self.geode,
+        }
+    }
+
     pub fn div_up(self, other: Resources) -> Option<i32> {
         let ore = div_up(self.ore, other.ore)?;
         let clay = div_up(self.clay, other.clay)?;
         let obsidian = div_up(self.obsidian, other.obsidian)?;
         let geode = div_up(self.geode, other.geode)?;
         Some(max(max(ore, clay), max(obsidian, geode)))
+    }
+
+    pub fn max(self, other: Resources) -> Resources {
+        Resources {
+            ore: max(self.ore, other.ore),
+            clay: max(self.clay, other.clay),
+            obsidian: max(self.obsidian, other.obsidian),
+            geode: max(self.geode, other.geode),
+        }
     }
 }
 
@@ -87,26 +106,32 @@ pub fn parse_blueprints(line: &str) -> (i32, Vec<Blueprint>) {
     (index, robots)
 }
 
-fn simulate_imp(best_seen: &mut Vec<i32>, blueprints: &[Blueprint], time_left: i32, production: Resources, resources: Resources) -> i32 {
+struct SimulationContext<'a> {
+    pub best_seen: Vec<i32>,
+    pub max_production: Resources,
+    pub blueprints: &'a [Blueprint],
+}
+
+fn simulate_imp(ctx: &mut SimulationContext, time_left: i32, production: Resources, resources: Resources) -> i32 {
     let mut best = resources.geode + production.geode * time_left;
     let potential = best + ((time_left - 1) * time_left) / 2;
-    if potential <= best_seen[time_left as usize] {
+    if potential <= ctx.best_seen[time_left as usize] {
         return best;
     }
 
-    for bp in blueprints {
+    for bp in ctx.blueprints {
         if let Some(wait_time) = (bp.cost - resources).div_up(production) {
             let time = wait_time + 1;
-            if time < time_left {
+            if time < time_left && production.get(bp.output) < ctx.max_production.get(bp.output) {
                 let res = resources + production * time - bp.cost;
                 let prod = production + Resources::of(bp.output, 1);
-                let obs = simulate_imp(best_seen, blueprints, time_left - time, prod, res);
+                let obs = simulate_imp(ctx, time_left - time, prod, res);
                 best = max(best, obs);
             }
         }
     }
 
-    let seen = &mut best_seen[time_left as usize];
+    let seen = &mut ctx.best_seen[time_left as usize];
     *seen = max(*seen, best);
 
     best
@@ -114,7 +139,13 @@ fn simulate_imp(best_seen: &mut Vec<i32>, blueprints: &[Blueprint], time_left: i
 
 pub fn simulate(blueprints: &[Blueprint], time_left: i32) -> i32 {
     let initial_produciton = Resources { ore: 1, ..Default::default() };
-    let mut best_seen = vec![0i32; time_left as usize + 1];
-
-    simulate_imp(&mut best_seen, blueprints, time_left, initial_produciton, Resources::default())
+    let max_production = blueprints.iter()
+        .map(|bp| bp.cost)
+        .fold(Resources::of(Resource::Geode, i32::MAX), Resources::max);
+    let mut ctx = SimulationContext {
+        best_seen: vec![0i32; time_left as usize + 1],
+        max_production: max_production,
+        blueprints: blueprints,
+    };
+    simulate_imp(&mut ctx, time_left, initial_produciton, Resources::default())
 }
